04f8617935268dd1c15011703f2736146a8fe29d
MightyPrytanis <davidtowne@gmail.com>
2025-12-29 07:21:53 -0500

    Initial Commit

---
+++ PrimePriceTests/PrimePriceTests.swift
@@ -0,0 +1,16 @@
+//
+//  PrimePriceTests.swift
+//  PrimePriceTests
+//
+//  Created by David Towne on 12/29/25.
+//
+
+import Testing
+
+struct PrimePriceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
